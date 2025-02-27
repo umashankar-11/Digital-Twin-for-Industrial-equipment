ec9d512a8a838bc2358dfd5c87208fc231562203
RN UMASHANKAR <umashankarrn.2002@gmail.com>
2025-02-27 17:18:03 +0530

    file

---
+++ adaptive_control_system.py
@@ -0,0 +1,21 @@
+class PIDController:
+    def __init__(self, kp, ki, kd, setpoint):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
+        self.prev_error = 0
+        self.integral = 0
+
+    def compute(self, current_value):
+        error = self.setpoint - current_value
+        self.integral += error
+        derivative = error - self.prev_error
+        output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        self.prev_error = error
+        return output
+
+pid = PIDController(kp=1.0, ki=0.1, kd=0.01, setpoint=100) 
+current_rpm = 90
+adjustment = pid.compute(current_rpm) 
+print(f"Adjustment: {adjustment}")
